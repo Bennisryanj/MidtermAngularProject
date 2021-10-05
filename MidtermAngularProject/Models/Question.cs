db975e7b1f06705b74537b77b6b92f167e75c7f2
Ryan.bennis <Buddybot180@gmail.com>
2021-10-04 21:25:56 -0400

    Added Favorites Controller

---
+++ MidtermAngularProject/Models/Question.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace MidtermAngularProject.Models
+{
+    public partial class Question
+    {
+        public Question()
+        {
+            Favorites = new HashSet<Favorite>();
+        }
+
+        public int QuestionId { get; set; }
+        public string QuestionText { get; set; }
+        public string AnswerText { get; set; }
+
+        public virtual ICollection<Favorite> Favorites { get; set; }
+    }
+}
---
+++ MidtermAngularProject/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace MidtermAngularProject.Models
+{
+    public partial class User
+    {
+        public User()
+        {
+            Favorites = new HashSet<Favorite>();
+        }
+
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public virtual ICollection<Favorite> Favorites { get; set; }
+    }
+}
