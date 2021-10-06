8c89026c976720b3d6b89df856a3ca26845356fa
clarke <cf.lamb25@gmail.com>
2021-10-06 18:40:01 -0400

    Add-Question

---
+++ MidtermAngularProject/Models/Favorite.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace MidtermAngularProject.Models
+{
+    public partial class Favorite
+    {
+        public int FavoriteId { get; set; }
+        public int? QuestionId { get; set; }
+        public int? UserId { get; set; }
+        public string QuestionText { get; set; }
+        public string AnswerText { get; set; }
+
+        public virtual Question Question { get; set; }
+        public virtual User User { get; set; }
+    }
+}
---
+++ MidtermAngularProject/Models/StudyBuddyDBContext.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace MidtermAngularProject.Models
+{
+    public partial class StudyBuddyDBContext : DbContext
+    {
+        public StudyBuddyDBContext()
+        {
+        }
+
+        public StudyBuddyDBContext(DbContextOptions<StudyBuddyDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Favorite> Favorites { get; set; }
+        public virtual DbSet<Question> Questions { get; set; }
+        public virtual DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=(LocalDb)\\LocalDB;Database=StudyBuddyDB;Trusted_Connection=true;MultipleActiveResultSets=true");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<Favorite>(entity =>
+            {
+                entity.Property(e => e.FavoriteId).HasColumnName("FavoriteID");
+
+                entity.Property(e => e.AnswerText)
+                    .HasMaxLength(500)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.QuestionId).HasColumnName("QuestionID");
+
+                entity.Property(e => e.QuestionText)
+                    .HasMaxLength(200)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.UserId).HasColumnName("UserID");
+
+                entity.HasOne(d => d.Question)
+                    .WithMany(p => p.Favorites)
+                    .HasForeignKey(d => d.QuestionId)
+                    .HasConstraintName("FK__Favorites__Quest__286302EC");
+
+                entity.HasOne(d => d.User)
+                    .WithMany(p => p.Favorites)
+                    .HasForeignKey(d => d.UserId)
+                    .HasConstraintName("FK__Favorites__UserI__29572725");
+            });
+
+            modelBuilder.Entity<Question>(entity =>
+            {
+                entity.Property(e => e.QuestionId).HasColumnName("QuestionID");
+
+                entity.Property(e => e.AnswerText)
+                    .HasMaxLength(500)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.QuestionText)
+                    .HasMaxLength(200)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.Property(e => e.FirstName)
+                    .HasMaxLength(200)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.LastName)
+                    .HasMaxLength(200)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
