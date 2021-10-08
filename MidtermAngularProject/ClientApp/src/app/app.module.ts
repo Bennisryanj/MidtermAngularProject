36a4ac60b6ae2c52e0406716d0244a7f769b0de8
kate <kcole0422@gmail.com>
2021-10-07 22:25:43 -0400

    fixed last merge conflict

--- MidtermAngularProject/ClientApp/src/app/app.module.ts
+++ MidtermAngularProject/ClientApp/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { FavoriteComponent } from './favorite/favorite.component';
+import { AddQuestionComponent } from './add-question/add-question.component';
 
 @NgModule({
   declarations: [
